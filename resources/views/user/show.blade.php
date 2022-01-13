775b5bb6c5d442afc8f665c33ebaa1e6c13207bf
t-devl <79603858+t-devl@users.noreply.github.com>
2022-01-13 12:13:01 +0000

    Update followers without refreshing page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -11,14 +11,14 @@
         @if(!$isFollowed)
         <form class="profile__follow" method="POST" action="/follow">
             @csrf
-            <input type="hidden" value="{{ $user->id }}" name="followedId" />
+            <input  type="hidden" class="follow__id" value="{{ $user->id }}" name="followedId" />
             <button class="profile__follow-button" type="submit">Follow</button>
         </form>
         @else
         <form class="profile__follow" method="POST" action="/follow">
             @csrf
             @method("delete")
-            <input type="hidden" value="{{ $user->id }}" name="followedId" />
+            <input type="hidden" class="follow__id" value="{{ $user->id }}" name="followedId" />
             <button class="profile__follow-button profile__follow-button--unfollow" type="submit">Unfollow</button>
         </form>
         @endif
@@ -26,11 +26,11 @@
     </div>
  
     <div class="profile__follow-stats">
-        <div class="profile__follow-stat">
-            <a class="profile__follow-link" href="/users/{{ $user->id }}/following">Following: <span class="profile__follow-number">{{ $followingCount }}</span></a>
+        <div class="profile__follow-stat profile__follow-stat--following">
+            <a class="profile__follow-link" href="/users/{{ $user->id }}/following">Following: <span class="profile__follow-count">{{ $followingCount }}</span></a>
         </div>
-        <div class="profile__follow-stat">
-            <a class="profile__follow-link" href="/users/{{ $user->id }}/followers">Followers: <span class="profile__follow-number">{{ $followersCount }}</span></a>
+        <div class="profile__follow-stat profile__follow-stat--followers">
+            <a class="profile__follow-link" href="/users/{{ $user->id }}/followers">Followers: <span class="profile__follow-count">{{ $followersCount }}</span></a>
         </div>
       
     </div>
@@ -91,6 +91,46 @@
                         ${$likesCount == 2 ? `<span class="post__like-count">1</span> like`
                         : `<span class="post__like-count">${$likesCount - 1}</span> likes`}  
                         `);
+                        $form.find("[name='_method']").remove();
+                    }
+                }
+            });
+        });
+
+        $(".profile__follow").on("submit", function(e){
+            e.preventDefault();
+            $form = $(e.target);
+            $button = $form.find(".profile__follow-button");
+            $button.attr("disabled", true);
+            $followId = $form.find(".follow__id").val();
+            $formData = $form.serialize();
+
+            if($form.find("[name='_method']").val()){
+                $method = "delete";
+            }
+            else{
+                $method = "post";
+            }
+
+            $.ajax({
+                url: "/follow",
+                type: "POST",
+                data: $formData,
+                success: function(res){
+                    $button.attr("disabled", false);
+                    $followers = $(document).find(".profile__follow-stat--followers");
+                    $followerCount = $followers.find(".profile__follow-count");
+
+                    if($method == "post"){
+                        $button.addClass("profile__follow-button--unfollow");
+                        $button.html("Unfollow");
+                        $followerCount.html(parseInt($followerCount.text()) + 1);
+                        $form.append("<input type='hidden' name='_method' value='delete' />");
+                    }
+                    else{
+                        $button.removeClass("profile__follow-button--unfollow");
+                        $button.html("Follow");
+                        $followerCount.html(parseInt($followerCount.text()) - 1);
                         $form.find("[name='_method']").remove();
                     }
                 }
